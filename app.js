d02e0059404c096ebbc65b368921b8fa3b14b9d2
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-03-01 03:57:18 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const searchResult = () =>{
     const searchBox = document.getElementById('search-box');
     const searchValue = searchBox.value;
+    searchValue.textContent =''
+    
     const url =`https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
     .then(res => res.json())
@@ -9,6 +11,7 @@ const searchResult = () =>{
 }
 const displayAllPhone = mobiles =>{
     const cardsContainer = document.getElementById('cards-container')
+    cardsContainer.textContent =''
     for(const mobile of mobiles.slice(0,20)){
         const div =document.createElement('div')
         div.classList.add('col')
@@ -33,5 +36,40 @@ const moreDetails = id =>{
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data))
+    .then(data => displayDetails(data.data))
+}
+
+const displayDetails = info =>{
+       const detailsBanner = document.getElementById('details-banner')
+       detailsBanner.textContent =''
+       const div = document.createElement('div')
+       div.innerHTML =`
+       <div class="card mx-auto" style="max-width: 540px;">
+       <div class="row">
+       <div class="col-md-5">
+       <img src="${info.image}" class="card-img-top" alt="...">
+       </div>
+       <div class="col-md-7">
+       <div class="card-body">
+         <h4 class="card-title">${info.name}</h4>
+         <p class="card-text"><small class="text-muted">${info.releaseDate}:'release-date not found'</small></p>
+         <h5>Main-features:</h5>
+         <p>
+         <strong>chipSet:</strong> ${info.mainFeatures.chipSet} <strong>display:</strong> ${info.mainFeatures.displaySize} <strong>memory:</strong> ${info.mainFeatures.memory}    
+         <strong>storage:</strong> ${info.mainFeatures.storage}
+         </P>
+         <p id ="sensors"><strong>Sensors: </strong></p>
+       </div>
+       </div>
+       </div>
+     </div>
+       `
+detailsBanner.appendChild(div)
+const sensors = document.getElementById('sensors')
+info.mainFeatures.sensors.forEach(feature =>{
+  const span = document.createElement('span')
+  span.innerText = `${feature}, `
+  sensors.appendChild(span) 
+})
+console.log(info)
 }
